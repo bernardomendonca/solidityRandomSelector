f644a4a5586a03572aafe6866dbe4f972a3f30c4
bernardo mendonça <bernardomendonca@outlook.com>
2021-09-23 16:41:19 +1000

    Contract boilerplate setup

---
+++ randomSelector.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.17;
+// ^ OK to ignore the red underline.
+
+// This contract allows a "manager" to start a contract to randomly select a person from a poll
+// The "manager" is whoemver deployed the contract
+
+contract Lottery {
+    //
+    address public manager;
+
+    function Lottery() public {
+        
+    }
+}
